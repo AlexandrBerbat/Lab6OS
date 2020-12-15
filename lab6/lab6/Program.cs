f491f8baa6876b303e4ef8f0c8a3547bc1a70ace
Alexandr Berbat <42623575+AlexandrBerbat@users.noreply.github.com>
2020-12-15 22:53:32 +0200

    Add files via upload

---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+public class Program{ 
+	static void Main(string[] args)
+{
+	Console.WriteLine("Inside main()");
+	string typeOfShape = Console.ReadLine();
+	int i = 0;
+	for (; i < 0xffffff; i++) ;
+	switch (typeOfShape)
+	{
+		case "dog":
+			DrawDog();
+			break;
+		case "cat":
+			DrawCat();
+			break;
+	}
+}
+static void DrawCat()
+{
+	for (int i = 0; i < 1000000000; i++)
+	{
+		//process of drawing
+	}
+
+}
+static void DrawDog()
+{
+	for (int i = 0; i < 1000000000; i++)
+	{
+		//process of drawing
+	}
+}
+}
+
